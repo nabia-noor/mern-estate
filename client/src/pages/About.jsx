f1b71db3ca827a877f120e881b0124203fc6e3fa
Nabia Noor <mtariq2270@gmail.com>
2025-05-22 19:24:36 +0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return <div className="py-20 px-4 max-w-6xl mx-auto">
+    <h1 className="text-3xl font-bold mb-4 text-slate-800">About Sahand Estate</h1>
+    <p className="mb-4 text-slate-700">Sahand Estate is a leading real estate agency that specialize in
+      in helping clients by, sell, and rent properties in the most desireable neighbourhood.
+      our team of experienced agent is dedicated to provided exceptional service and making  the buying and selling process as smooth as possible.</p>
+      <p className="mb-4 text-slate-700">Our mission is to help our clients acheive this real estate goals by providing experts advice and a deep understanding of the 
+        local market
+    </p>
+    <p className="mb-4 text-slate-700">
+      our team of agents has a wealth of experience and knowldge in the real estate industry and we are commited to providing the
+       highest level of service to the clients we believe thst buying or selling  the property should be an exciting and rewarding experience.
+    </p>
+  </div>;
 }
